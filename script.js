e3afe6159c157cb394e9fdab4c0d79e3b383fc4f
patrickb <patrick.belaire@gmail.com>
2015-12-14 16:53:17 +0100

    jquery_1

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('#deroulerHaut').click(function() {
+   $('#masque').slideUp();
+   $("#deroulerBas").click(function() {
+ $("#masque").slideDown();
+ });
+   });
+   $(document).ready(function(){
+ $("button").click(function(){
+ $("p").toggle();
+ });
+});
+});
